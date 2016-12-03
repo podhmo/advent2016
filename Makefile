c2565268e4f80ebf2a7c4b6dbed32bb264246616
podhmo <ababjam61+github@gmail.com>
2016-12-03 19:05:36 +0900

    fizzbuzz

--- Makefile
+++ Makefile
@@ -3,6 +3,15 @@ hello:
 	python src/basic/00hello.py | gofmt > dst/basic/hello/main.go
 	go run dst/basic/hello/main.go
 
+cross:
+	mkdir -p dst/basic/cross
+	python src/basic/01cross*.py | gofmt > dst/basic/cross/cross2.go
+	python src/basic/02cross*.py | gofmt > dst/basic/cross/cross5.go
+
+fizzbuzz:
+	mkdir -p dst/basic/fizzbuzz
+	python src/basic/03*.py | gofmt > dst/basic/fizzbuzz/main.go
+	go run dst/basic/fizzbuzz/main.go
+
 setup:
 	pip install -r requirements.txt
-
---
+++ src/basic/03fizzbuzz.py
@@ -0,0 +1,32 @@
+from prestring.go import Module
+m = Module()
+m.package('main')
+
+with m.import_group() as im:
+    im.import_("fmt")
+
+with m.func('main'):
+    with m.for_("i := 1; i <= 100; i++"):
+        m.stmt("v, err := FizzBuzz(i)")
+        with m.if_("err != nil"):
+            m.stmt("panic(err)")
+        m.stmt("fmt.Println(v)")
+
+
+with m.func('FizzBuzz', 'v int', return_='(string, error)'):
+    with m.if_("v == 1"):
+        m.return_('"1", nil')
+    for i in range(2, 101):
+        with m.elif_("v == {}".format(i)):
+            if i % 15 == 0:
+                m.return_('"fizzbuzz", nil')
+            elif i % 3 == 0:
+                m.return_('"fizz", nil')
+            elif i % 5 == 0:
+                m.return_('"buzz", nil')
+            else:
+                m.return_('"{}", nil'.format(i))
+    with m.else_():
+        m.return_('"", fmt.Errorf("unsupported value: %q", v)')
+
+print(m)
