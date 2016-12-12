6d0fbc4c3eb3c526707757a48091337480b57e34
podhmo <ababjam61+github@gmail.com>
2016-12-12 23:16:10 +0900

    hmm

--- dst/swagger/convert/autogen.go
+++ dst/swagger/convert/autogen.go
@@ -2,59 +2,63 @@ package convert
 
 import (
 	def "github.com/podhmo/advent2016/dst/swagger/gen/def"
-	autogen "github.com/podhmo/advent2016/src/github"
+	github "github.com/podhmo/advent2016/src/github"
 )
 
 // AuthenticatedUserToUser :
-func AuthenticatedUserToUser(from *autogen.AuthenticatedUser) *def.User {
+func AuthenticatedUserToUser(from *github.AuthenticatedUser) *def.User {
 	to := &def.User{}
-	to.AvatarURL = from.AvatarURL
+	tmp1 := (string)(from.AvatarURL)
+	to.AvatarURL = tmp1
 	to.Bio = from.Bio
-	to.Blog = from.Blog
-	tmp1 := (int64)(from.Collaborators)
-	to.Collaborators = tmp1
+	tmp2 := (string)(from.Blog)
+	to.Blog = tmp2
+	tmp3 := (int64)(from.Collaborators)
+	to.Collaborators = tmp3
 	to.Company = from.Company
-	to.CreatedAt = from.CreatedAt
-	tmp2 := (int64)(from.DiskUsage)
-	to.DiskUsage = tmp2
+	to.CreatedAt = TimeToString(from.CreatedAt)
+	tmp4 := (int64)(from.DiskUsage)
+	to.DiskUsage = tmp4
 	to.Email = from.Email
-	tmp3 := (int64)(from.Followers)
-	to.Followers = tmp3
-	tmp4 := (int64)(from.Following)
-	to.Following = tmp4
+	tmp5 := (int64)(from.Followers)
+	to.Followers = tmp5
+	tmp6 := (int64)(from.Following)
+	to.Following = tmp6
 	to.GravatarID = from.GravatarID
 	to.Hireable = from.Hireable
-	to.HTMLURL = from.HTMLURL
-	tmp5 := (int64)(from.ID)
-	to.ID = tmp5
+	tmp7 := (string)(from.HTMLURL)
+	to.HTMLURL = tmp7
+	tmp8 := (int64)(from.ID)
+	to.ID = tmp8
 	to.Location = from.Location
 	to.Login = from.Login
 	to.Name = from.Name
-	tmp6 := (int64)(from.OwnedPrivateRepos)
-	to.OwnedPrivateRepos = tmp6
+	tmp9 := (int64)(from.OwnedPrivateRepos)
+	to.OwnedPrivateRepos = tmp9
 	to.Plan = PlanToUserPlan(&(from.Plan))
-	tmp7 := (int64)(from.PrivateGists)
-	to.PrivateGists = tmp7
-	tmp8 := (int64)(from.PublicGists)
-	to.PublicGists = tmp8
-	tmp9 := (int64)(from.PublicRepos)
-	to.PublicRepos = tmp9
-	tmp10 := (int64)(from.TotalPrivateRepos)
-	to.TotalPrivateRepos = tmp10
+	tmp10 := (int64)(from.PrivateGists)
+	to.PrivateGists = tmp10
+	tmp11 := (int64)(from.PublicGists)
+	to.PublicGists = tmp11
+	tmp12 := (int64)(from.PublicRepos)
+	to.PublicRepos = tmp12
+	tmp13 := (int64)(from.TotalPrivateRepos)
+	to.TotalPrivateRepos = tmp13
 	to.Type = from.Type
-	to.URL = from.URL
+	tmp14 := (string)(from.URL)
+	to.URL = tmp14
 	return to
 }
 
 // PlanToUserPlan :
-func PlanToUserPlan(from *autogen.Plan) *def.UserPlan {
+func PlanToUserPlan(from *github.Plan) *def.UserPlan {
 	to := &def.UserPlan{}
-	tmp11 := (int64)(from.Collaborators)
-	to.Collaborators = tmp11
+	tmp15 := (int64)(from.Collaborators)
+	to.Collaborators = tmp15
 	to.Name = from.Name
-	tmp12 := (int64)(from.PrivateRepos)
-	to.PrivateRepos = tmp12
-	tmp13 := (int64)(from.Space)
-	to.Space = tmp13
+	tmp16 := (int64)(from.PrivateRepos)
+	to.PrivateRepos = tmp16
+	tmp17 := (int64)(from.Space)
+	to.Space = tmp17
 	return to
 }
--- src/github/authenticated_user.go
+++ src/github/authenticated_user.go
@@ -1,6 +1,7 @@
 package github
 
 import (
+	"github.com/go-openapi/strfmt"
 	"time"
 )
 
@@ -10,42 +11,42 @@ AuthenticatedUser
 */
 // AuthenticatedUser : auto generated JSON container
 type AuthenticatedUser struct {
-	AvatarURL         string    `json:"avatar_url" example:"https://github.com/images/error/octocat_happy.gif"`
-	Bio               string    `json:"bio" example:"There once was..."`
-	Blog              string    `json:"blog" example:"https://github.com/blog"`
-	Collaborators     int       `json:"collaborators" example:"8"`
-	Company           string    `json:"company" example:"GitHub"`
-	CreatedAt         time.Time `json:"created_at" example:"2008-01-14T04:33:35Z"`
-	DiskUsage         int       `json:"disk_usage" example:"10000"`
-	Email             string    `json:"email" example:"octocat@github.com"`
-	EventsURL         string    `json:"events_url" example:"https://api.github.com/users/octocat/events{/privacy}"`
-	Followers         int       `json:"followers" example:"20"`
-	FollowersURL      string    `json:"followers_url" example:"https://api.github.com/users/octocat/followers"`
-	Following         int       `json:"following" example:"0"`
-	FollowingURL      string    `json:"following_url" example:"https://api.github.com/users/octocat/following{/other_user}"`
-	GistsURL          string    `json:"gists_url" example:"https://api.github.com/users/octocat/gists{/gist_id}"`
-	GravatarID        string    `json:"gravatar_id" example:""`
-	HTMLURL           string    `json:"html_url" example:"https://github.com/octocat"`
-	Hireable          bool      `json:"hireable" example:"False"`
-	ID                int       `json:"id" example:"1"`
-	Location          string    `json:"location" example:"San Francisco"`
-	Login             string    `json:"login" example:"octocat"`
-	Name              string    `json:"name" example:"monalisa octocat"`
-	OrganizationsURL  string    `json:"organizations_url" example:"https://api.github.com/users/octocat/orgs"`
-	OwnedPrivateRepos int       `json:"owned_private_repos" example:"100"`
-	Plan              Plan      `json:"plan"`
-	PrivateGists      int       `json:"private_gists" example:"81"`
-	PublicGists       int       `json:"public_gists" example:"1"`
-	PublicRepos       int       `json:"public_repos" example:"2"`
-	ReceivedEventsURL string    `json:"received_events_url" example:"https://api.github.com/users/octocat/received_events"`
-	ReposURL          string    `json:"repos_url" example:"https://api.github.com/users/octocat/repos"`
-	SiteAdmin         bool      `json:"site_admin" example:"False"`
-	StarredURL        string    `json:"starred_url" example:"https://api.github.com/users/octocat/starred{/owner}{/repo}"`
-	SubscriptionsURL  string    `json:"subscriptions_url" example:"https://api.github.com/users/octocat/subscriptions"`
-	TotalPrivateRepos int       `json:"total_private_repos" example:"100"`
-	Type              string    `json:"type" example:"User"`
-	URL               string    `json:"url" example:"https://api.github.com/users/octocat"`
-	UpdatedAt         time.Time `json:"updated_at" example:"2008-01-14T04:33:35Z"`
+	AvatarURL         strfmt.URI `json:"avatar_url" example:"https://github.com/images/error/octocat_happy.gif"`
+	Bio               string     `json:"bio" example:"There once was..."`
+	Blog              strfmt.URI `json:"blog" example:"https://github.com/blog"`
+	Collaborators     int        `json:"collaborators" example:"8"`
+	Company           string     `json:"company" example:"GitHub"`
+	CreatedAt         time.Time  `json:"created_at" example:"2008-01-14T04:33:35Z"`
+	DiskUsage         int        `json:"disk_usage" example:"10000"`
+	Email             string     `json:"email" example:"octocat@github.com"`
+	EventsURL         strfmt.URI `json:"events_url" example:"https://api.github.com/users/octocat/events{/privacy}"`
+	Followers         int        `json:"followers" example:"20"`
+	FollowersURL      strfmt.URI `json:"followers_url" example:"https://api.github.com/users/octocat/followers"`
+	Following         int        `json:"following" example:"0"`
+	FollowingURL      strfmt.URI `json:"following_url" example:"https://api.github.com/users/octocat/following{/other_user}"`
+	GistsURL          strfmt.URI `json:"gists_url" example:"https://api.github.com/users/octocat/gists{/gist_id}"`
+	GravatarID        string     `json:"gravatar_id" example:""`
+	HTMLURL           strfmt.URI `json:"html_url" example:"https://github.com/octocat"`
+	Hireable          bool       `json:"hireable" example:"False"`
+	ID                int        `json:"id" example:"1"`
+	Location          string     `json:"location" example:"San Francisco"`
+	Login             string     `json:"login" example:"octocat"`
+	Name              string     `json:"name" example:"monalisa octocat"`
+	OrganizationsURL  strfmt.URI `json:"organizations_url" example:"https://api.github.com/users/octocat/orgs"`
+	OwnedPrivateRepos int        `json:"owned_private_repos" example:"100"`
+	Plan              Plan       `json:"plan"`
+	PrivateGists      int        `json:"private_gists" example:"81"`
+	PublicGists       int        `json:"public_gists" example:"1"`
+	PublicRepos       int        `json:"public_repos" example:"2"`
+	ReceivedEventsURL strfmt.URI `json:"received_events_url" example:"https://api.github.com/users/octocat/received_events"`
+	ReposURL          strfmt.URI `json:"repos_url" example:"https://api.github.com/users/octocat/repos"`
+	SiteAdmin         bool       `json:"site_admin" example:"False"`
+	StarredURL        strfmt.URI `json:"starred_url" example:"https://api.github.com/users/octocat/starred{/owner}{/repo}"`
+	SubscriptionsURL  strfmt.URI `json:"subscriptions_url" example:"https://api.github.com/users/octocat/subscriptions"`
+	TotalPrivateRepos int        `json:"total_private_repos" example:"100"`
+	Type              string     `json:"type" example:"User"`
+	URL               strfmt.URI `json:"url" example:"https://api.github.com/users/octocat"`
+	UpdatedAt         time.Time  `json:"updated_at" example:"2008-01-14T04:33:35Z"`
 }
 
 // Plan : auto generated JSON container
